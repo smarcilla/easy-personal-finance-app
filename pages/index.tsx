201f2edb01c4fe9187b94a05251886e3eb4945d6
g_garsan@hotmail.com <g_garsan@hotmail.com>
2023-01-17 17:47:16 +0100

    setup tailwindcss added

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,15 +6,19 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json())
 export default function Index() {
   const { data, error, isLoading } = useSWR<FinanceTransaction[]>('/api/finance', fetcher)
 
-  if (error) return <div>Failed to load</div>
-  if (isLoading) return <div>Loading...</div>
+  if (error) return <div className="text-3xl font-bold underline">Failed to load</div>
+  if (isLoading) return <div className="text-3xl font-bold underline">Loading...</div>
   if (!data) return null
 
   return (
-    <ul>
+        <>
+        <div className="text-3xl font-bold text-center">Easy Personal Finance App</div>
+         <ul className='text-center'>
       {data.map((ft:FinanceTransaction) => (
         <li key={ft.type}>{ft.type}</li>
       ))}
     </ul>
+        </>
+   
   )
 }
