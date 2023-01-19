519682ffcf54af68a1b0906c4f1e00895f7727e0
g_garsan@hotmail.com <g_garsan@hotmail.com>
2023-01-19 19:20:28 +0100

    feat: FinancialTransaction component added

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,15 +19,13 @@ export default function Index() {
         <div className="text-3xl font-bold text-center">Easy Personal Finance App</div>
         <br/>
         <div className="bg-gray-200 text-gray-700 text-sm font-medium uppercase tracking-wider text-center border-b border-gray-300">
-        <div className="flex flex-wrap items-center justify-between">
-        <div className="w-1/5 p-2">Date</div>
-        <div className="w-1/5 p-2">Concept</div>
-        <div className="w-1/5 p-2">Movement</div>
-        <div className="w-1/5 p-2">Amount</div>
-        <div className="w-1/5 p-2">Notes</div>
-</div>
-
-        
+                <div className="flex flex-wrap items-center justify-between">
+                        <div className="w-1/5 p-2">Date</div>
+                        <div className="w-1/5 p-2">Concept</div>
+                        <div className="w-1/5 p-2">Movement</div>
+                        <div className="w-1/5 p-2">Amount</div>
+                        <div className="w-1/5 p-2">Notes</div>
+                </div>
         </div>
 
          <ul className='text-center'>
