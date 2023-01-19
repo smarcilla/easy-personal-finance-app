4eb9894b2421709363ac30e3b8ba164d9f58ff78
g_garsan@hotmail.com <g_garsan@hotmail.com>
2023-01-19 19:12:04 +0100

    feat: FinancialTransaction component added

---
+++ pages/FinancialTransaction.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+interface Props {
+  transaction: {
+    date: Date;
+    concept: string;
+    movement: string;
+    amount: number;
+    notes: string;
+  };
+}
+
+const FinancialTransaction: React.FC<Props> = ({ transaction }) => {
+  return (
+    <div className="border border-black">
+      <div className="flex">
+        <div className="border-solid border-black w-1/5 p-2">{transaction.date}</div>
+        <div className="border-solid border-black w-1/5 p-2">{transaction.concept}</div>
+        <div className="border-solid border-black w-1/5 p-2">{transaction.movement}</div>
+        <div className="border-solid border-black w-1/5 p-2">{transaction.amount}</div>
+        <div className="border-solid border-black w-1/5 p-2">{transaction.notes}</div>
+      </div>
+    </div>
+  );
+};
+
+export default FinancialTransaction;
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,24 +17,23 @@ export default function Index() {
   return (
         <>
         <div className="text-3xl font-bold text-center">Easy Personal Finance App</div>
-        <br></br>
-        <table className='w-full'>
-        <thead className="bg-gray-200 text-gray-700 text-sm font-medium uppercase tracking-wider text-center border-b border-gray-300">
-        <tr >
-        <th>Date</th>
-        <th>Concept</th>
-        <th>Movement</th>
-        <th>Amount</th>
-        <th>Type</th>
-        <th>Notes</th>
-        </tr>
-        </thead>
-        </table>
+        <br/>
+        <div className="bg-gray-200 text-gray-700 text-sm font-medium uppercase tracking-wider text-center border-b border-gray-300">
+        <div className="flex flex-wrap items-center justify-between">
+        <div className="w-1/5 p-2">Date</div>
+        <div className="w-1/5 p-2">Concept</div>
+        <div className="w-1/5 p-2">Movement</div>
+        <div className="w-1/5 p-2">Amount</div>
+        <div className="w-1/5 p-2">Notes</div>
+</div>
+
+        
+        </div>
 
          <ul className='text-center'>
                  {data.map((ft:FinanceTransaction) => (
                          <li key={ft.id}>
-                        <FinancialTransaction transaction={ft}/>
+                        <FinancialTransaction transaction={{ date: ft.date, concept: ft.concept,movement: ft.movement,amount: ft.amount, notes: ft.notes}} />
                         </li>
                 ))}
         </ul>
