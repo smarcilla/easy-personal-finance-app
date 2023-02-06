a3b7e06fef656c8bda2cd8e23046ad877d4f1bfc
g_garsan@hotmail.com <g_garsan@hotmail.com>
2023-02-06 20:51:24 +0100

    feat(lib): lib deleted

--- lib/converters/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { FinanceDataType } from '../types/finance.types'
-export declare class FinanceDataConverter {
-  private file
-  constructor(originCsvFile: string)
-  convert(): FinanceDataType[]
-}
--- lib/mappers/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { CsvToJsonDataType, FinanceDataType } from '../types/finance.types'
-export declare class FinanceMapper {
-  private readonly data
-  constructor(data: CsvToJsonDataType)
-  map(): FinanceDataType
-}
