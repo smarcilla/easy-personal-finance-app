363f5c2727066dfdd8448cf5c216737df0268f39
g_garsan@hotmail.com <g_garsan@hotmail.com>
2023-02-20 16:49:24 +0100

    feat(notifications): toasts added and responsive web design

--- components/TransactionComponents/TransactionList.tsx
+++ components/TransactionComponents/TransactionList.tsx
@@ -8,22 +8,25 @@ type TransactionListProps = {
 
 const TransactionList: React.FC<TransactionListProps> = ({ transactions }) => {
   return (
-    <div className="grid grid-cols-1 p-6 md:grid-cols-4 lg:grid-cols-5">
-      <div className="col-span-1 md:col-span-1 lg:col-span-1 py-4 px-6 bg-gray-200 font-medium rounded-tl">
-        Date
+    <>
+      <div className="grid grid-cols-5  p-6 md:grid-cols-5 lg:grid-cols-5">
+        <div className="text-center col-span-1 md:col-span-1 lg:col-span-1 py-4 px-6 bg-gray-200  rounded-tl text-base sx:text-sx sm:text-sm lg:text-base">
+          Date
+        </div>
+        <div className="text-center col-span-1 md:col-span-1 lg:col-span-1 py-4 px-6 bg-gray-200 text-base sx:text-sx sm:text-sm lg:text-base">
+          Concept
+        </div>
+        <div className="text-center col-span-1 md:col-span-1 lg:col-span-1 py-4 px-6 bg-gray-200 text-base sx:text-sx sm:text-sm lg:text-base">
+          Movement
+        </div>
+        <div className="text-center col-span-1 md:col-span-1 lg:col-span-1 py-4 px-6 bg-gray-200 text-base sx:text-sx sm:text-sm lg:text-base">
+          Amount
+        </div>
+        <div className="text-center col-span-1 md:col-span-1 lg:col-span-1 py-4 px-6 bg-gray-200  rounded-tr text-base sx:text-sx sm:text-sm lg:text-base">
+          Notes
+        </div>
       </div>
-      <div className="col-span-1 md:col-span-1 lg:col-span-1 py-4 px-6 bg-gray-200 font-medium">
-        Concept
-      </div>
-      <div className="col-span-1 md:col-span-1 lg:col-span-1 py-4 px-6 bg-gray-200 font-medium">
-        Movement
-      </div>
-      <div className="col-span-1 md:col-span-1 lg:col-span-1 py-4 px-6 bg-gray-200 font-medium">
-        Amount
-      </div>
-      <div className="col-span-1 md:col-span-1 lg:col-span-1 py-4 px-6 bg-gray-200 font-medium rounded-tr">
-        Notes
-      </div>
+
       {transactions.map((transaction) => (
         <div
           key={`${transaction.date}-${transaction.concept}`}
@@ -32,7 +35,7 @@ const TransactionList: React.FC<TransactionListProps> = ({ transactions }) => {
           <Transaction transaction={transaction} />
         </div>
       ))}
-    </div>
+    </>
   )
 }
 
